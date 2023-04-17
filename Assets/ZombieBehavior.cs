1a607ceb288368761eb1f57bf1f6a96e1d624d99
C0nee <jakubczajkowski04@wp.pl>
2023-04-17 16:03:57 +0200

    jeden dziala drugi nie trzeci moonwalkk

--- Assets/ZombieBehavior.cs
+++ Assets/ZombieBehavior.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
 using UnityEngine;
@@ -6,6 +6,10 @@ using UnityEngine.AI;
 
 public class ZombieBehavior : MonoBehaviour
 {
+    private bool playerVisible = false;
+    private bool playerHearable = false;
+    public float hearRange = 5f;
+    public float sightRange = 15f;
     int hp = 10;
     GameObject player;
     NavMeshAgent agent;
@@ -19,26 +23,45 @@ public class ZombieBehavior : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
-        if(hp>0) { 
-        //transform.LookAt(player.transform.position);
-        //Vector3 playerDirection = transform.position - player.transform.position;
-
-        //transform.Translate(Vector3.forward * Time.deltaTime);
-        agent.destination= player.transform.position;}
-        else
-        {
-            agent.isStopped= true;
-        }
-        Vector3 raysource = transform.position + Vector3.up *1.8f;
-        Vector3 rayDest = player.transform.position + Vector3.up * 1.8f;
+        //sprawdz czy "widzimy" gracza
+        //jeœli twoje zombiaki maj¹wspó³rzêdn¹ y = 0 to musisz wzi¹æ poprawkê na wysokoœæ wzroku
+        Vector3 raySource = transform.position + Vector3.up * 0.8f;
+        Vector3 rayDirection = player.transform.position - transform.position;
+        UnityEngine.Debug.DrawRay(raySource, rayDirection);
+        //Debug.DrawRay(raySource, rayDirection);
+        //deklarujemy zmienn¹ na to w co trafi raycast
         RaycastHit hit;
-        if(Physics.Raycast(raysource,rayDest,out hit, 5f))
+        if (Physics.Raycast(raySource, rayDirection, out hit, sightRange))
         {
+            UnityEngine.Debug.Log(hit.transform.gameObject.name.ToString());
+            //uruchomi siê wtedy i tylko wtedy jeœli raycast cokolwiek trafi
             if (hit.transform.CompareTag("Player"))
+                playerVisible = true;
+            else
+                playerVisible = false;
+        }
+        //sprawdzamy czy słyszy
+        Collider[] heardObjects = Physics.OverlapSphere(transform.position, hearRange);
+        //sprawdz wszystkie kolajdery i jesli najdziesz gracza ustaw flage
+        playerHearable = false;
+        foreach(Collider collider in heardObjects)
+        {
+            if (collider.gameObject.CompareTag("Player"))
             {
-              UnityEngine.Debug.Log("WIdze");
+                playerHearable = true;
             }
+        }
+
+        //jeœli widzi gracza to idzie
+        agent.isStopped = !playerVisible && !playerHearable;
+        if (hp > 0)
+        {
+            //transform.LookAt(player.transform.position);
+            //Vector3 playerDirection = transform.position - player.transform.position;
+
+            //transform.Translate(Vector3.forward * Time.deltaTime);
+
+            agent.destination = player.transform.position;
         }
     }
     private void OnCollisionEnter(Collision collision)
